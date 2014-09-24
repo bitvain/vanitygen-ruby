a665025914247838775602a7bfcf1cc033534453
fengb <contact@fengb.info>
2014-09-24 18:29:19 -0500

    Continous mode

--- lib/vanitygen.rb
+++ lib/vanitygen.rb
@@ -4,4 +4,12 @@ module Vanitygen
   def self.generate(pattern)
     pattern
   end
+
+  def self.continuous(pattern)
+    raise unless block_given?
+
+    Thread.new do
+      loop { yield generate(pattern) }
+    end
+  end
 end
--- spec/vanitygen_spec.rb
+++ spec/vanitygen_spec.rb
@@ -1,10 +1,33 @@
 require 'vanitygen'
 
 describe Vanitygen do
+  let(:pattern_string) { '1ab' }
+  let(:pattern_any) { '1' }
+
   describe '.generate' do
-    describe 'with string argument' do
-      subject { Vanitygen.generate('1ab') }
-      it { is_expected.to start_with(string_pattern) }
+    context 'with string argument' do
+      subject { Vanitygen.generate(pattern_string) }
+      it { is_expected.to start_with(pattern_string) }
+    end
+  end
+
+  describe '.continuous' do
+    it 'requires a block' do
+      expect{Vanitygen.continuous(pattern_string)}.to raise_error
+    end
+
+    context 'with block' do
+      let(:yields) { [] }
+      subject! do
+        Vanitygen.continuous(pattern_any) { |key| yields << key }
+      end
+      after { subject.kill }
+
+      it { is_expected.to be_instance_of Thread }
+      it 'runs a lot' do
+        sleep 0.01
+        expect(yields.count).to be > 100
+      end
     end
   end
 end
