421095cffc94ace2011c5a6d2909ea86aebbc28c
fengb <contact@fengb.info>
2014-10-30 23:53:24 -0500

    Support multiple patterns

--- lib/vanitygen.rb
+++ lib/vanitygen.rb
@@ -1,21 +1,25 @@
 require "vanitygen/version"
 
 module Vanitygen
-  def self.generate(pattern)
+  def self.generate(*patterns)
+    raise ArgumentError.new('wrong number of arguments (0 for 1+)') unless patterns.size > 0
+
     loop do
       key = Bitcoin::Key.generate
-      if key.addr.start_with?(pattern)
+      if patterns.any? { |pattern| key.addr.start_with?(pattern) }
         return { address: key.addr, priv_key: key.priv }
       end
     end
   end
 
-  def self.continuous(pattern, options={})
+  def self.continuous(*patterns)
     raise LocalJumpError.new('no block given') unless block_given?
+
+    options = patterns.last.is_a?(Hash) ? patterns.pop : {}
     iters = options.fetch(:iters, Float::INFINITY)
 
-    while (iters -= 1) >= 0 do
-      yield generate(pattern)
+    while (iters -= 1) >= 0
+      yield generate(*patterns)
     end
   end
 
--- spec/vanitygen_spec.rb
+++ spec/vanitygen_spec.rb
@@ -2,24 +2,34 @@ require 'vanitygen'
 require 'bitcoin'
 
 describe Vanitygen do
-  let(:pattern_string) { '1A' }
+  let(:pattern_string_a) { '1A' }
+  let(:pattern_string_b) { '1B' }
   let(:pattern_any) { '1' }
 
   describe '.generate' do
     context 'with string argument' do
-      subject { Vanitygen.generate(pattern_string) }
+      subject { Vanitygen.generate(pattern_string_a) }
 
       it 'has valid address' do
         expect(subject[:address]).to satisfy { |addr| Bitcoin.valid_address?(addr) }
       end
 
       it 'has address starting with pattern' do
-        expect(subject[:address]).to start_with(pattern_string)
+        expect(subject[:address]).to start_with(pattern_string_a)
       end
 
       it 'has correct private key to unlock pattern' do
         bkey = Bitcoin::Key.new(subject[:priv_key])
         expect(subject[:address]).to eq(bkey.addr)
+      end
+    end
+
+    context 'with many string arguments' do
+      it 'can be either 1A or 1B' do
+        10.times do
+          data = Vanitygen.generate(pattern_string_a, pattern_string_b)
+          expect(data[:address]).to start_with(pattern_string_a) | start_with(pattern_string_b)
+        end
       end
     end
   end
@@ -32,27 +42,27 @@ describe Vanitygen do
     context 'with block' do
       it 'runs a lot' do
         yields = []
-        Vanitygen.continuous(pattern_any, iters: 10) { |key| yields << key }
+        Vanitygen.continuous(pattern_any, iters: 10) { |data| yields << data }
         expect(yields.count).to be 10
       end
 
       it 'returns valid addresses' do
         yields = []
-        Vanitygen.continuous(pattern_any, iters: 4) { |key| yields << key }
-        expect(yields).to all(satisfy { |key| Bitcoin.valid_address?(key[:address]) })
+        Vanitygen.continuous(pattern_any, iters: 4) { |data| yields << data }
+        expect(yields).to all(satisfy { |data| Bitcoin.valid_address?(data[:address]) })
       end
 
       it 'starts with matching pattern' do
         yields = []
-        Vanitygen.continuous(pattern_string, iters: 2) { |key| yields << key }
-        expect(yields).to all(satisfy { |key| key[:address].start_with?(pattern_string)} )
+        Vanitygen.continuous(pattern_string_a, iters: 2) { |data| yields << data }
+        expect(yields).to all(satisfy { |data| data[:address].start_with?(pattern_string_a)} )
       end
     end
   end
 
   describe '.difficulty' do
     it 'returns difficulty in integer' do
-      expect(Vanitygen.difficulty(pattern_string)).to be_a Integer
+      expect(Vanitygen.difficulty(pattern_string_a)).to be_a Integer
     end
   end
 end
