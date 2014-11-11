8c13e8a55857d2c2ddd8b5b1de1542a5ff7a01f6
fengb <contact@fengb.info>
2014-11-11 08:08:49 -0600

    Dynamically check headers

--- ext/vanitygen/extconf.rb
+++ ext/vanitygen/extconf.rb
@@ -1,9 +1,22 @@
 require 'mkmf'
+require 'set'
 
-have_library 'pcre'
-have_library 'crypto'
-have_library 'm'
-have_library 'pthread'
+headers = Set.new
+Dir[File.expand_path '../*', __FILE__].each do |filename|
+  File.open filename do |file|
+    headers.merge file.grep(/^\#include *</).map { |l| l.gsub(/^\#include *<(.*)>\s*$/, '\1') }
+  end
+end
+headers.to_a.sort.each do |header|
+  have_header header
+end
+
+File.open(File.expand_path '../../../vanitygen-c/Makefile', __FILE__) do |makefile|
+  libs = makefile.grep(/^LIBS=.*$/).first.gsub(/(LIBS=| )-l/, ' ').split
+  libs.sort.each do |lib|
+    have_library lib
+  end
+end
 
 $CFLAGS << " -std=c99"
 
