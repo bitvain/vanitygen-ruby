48e83c1e66ed8e242354050365a34988bff8b362
fengb <contact@fengb.info>
2014-11-11 21:34:23 -0600

    Refactored extconf

--- ext/vanitygen/extconf.rb
+++ ext/vanitygen/extconf.rb
@@ -1,21 +1,28 @@
 require 'mkmf'
 require 'set'
 
-headers = Set.new
-Dir[File.expand_path '../*.{h,c}', __FILE__].each do |filename|
-  File.open filename do |file|
-    headers.merge file.grep(/^\#include *</).map { |l| l.gsub(/^\#include *<(.*)>\s*$/, '\1') }
+def required_headers
+  Set.new.tap do |ret|
+    Dir[File.expand_path '../*.{h,c}', __FILE__].each do |filename|
+      File.open filename do |file|
+        ret.merge file.grep(/^\#include *</).map { |l| l.gsub(/^\#include *<(.*)>\s*$/, '\1') }
+      end
+    end
   end
 end
-headers.to_a.sort.each do |header|
+
+def required_libs
+  File.open(File.expand_path '../../../vanitygen-c/Makefile', __FILE__) do |makefile|
+    makefile.grep(/^LIBS=.*$/).first.gsub(/(LIBS=| )-l/, ' ').split
+  end
+end
+
+required_headers.sort.each do |header|
   have_header header
 end
 
-File.open(File.expand_path '../../../vanitygen-c/Makefile', __FILE__) do |makefile|
-  libs = makefile.grep(/^LIBS=.*$/).first.gsub(/(LIBS=| )-l/, ' ').split
-  libs.sort.each do |lib|
-    have_library lib
-  end
+required_libs.sort.each do |lib|
+  have_library lib
 end
 
 $CFLAGS << " -std=c99"
